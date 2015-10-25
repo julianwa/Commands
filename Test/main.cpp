0667e3228ff5817cc2f8a7f57fe549f1f43a2df2
Julian Walker <julian@fiftythree.com>
2015-10-24 20:22:53 -0400

    Quick tidy

--- Test/main.cpp
+++ Test/main.cpp
@@ -19,11 +19,11 @@ int main(int argc, const char * argv[])
     // 1) You can execute a typed command directly on the receiver
     receiver->Execute(make_shared<CommandA>());
     
-    // This would result in a static compile-time error because the receiver does
-    // not support CommmandD
+    // 2) This would result in a static compile-time error because the receiver does
+    //    not support CommmandD
     //receiver->Execute(make_shared<CommandD>());
     
-    // 2) Or you can execute generic commands on the receiver. However, in this case
+    // 3) Or you can execute generic commands on the receiver. However, in this case
     //    you don't know whether the receiver supports the command until run-time.
     auto commands = vector<shared_ptr<Command>> {
         make_shared<CommandB>(),
@@ -33,7 +33,7 @@ int main(int argc, const char * argv[])
         receiver->Execute(command);
     }
     
-    // 3) Or, in situations where the receiver's type needs to be factored out (for example,
+    // 4) Or, in situations where the receiver's type needs to be factored out (for example,
     //    if you wanted to create a map of reciever id -> receiver), then you can use the
     //    OpaqueCommandReceiver helper.
     auto opaqueReceiver = OpaqueCommandReceiver(receiver);
